9986880479c035205b7a6e635d90088dc806d22f
philnoug <philnoug@penguin>
2020-10-24 05:42:48 +0200

    Cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     if current_user 
       @groupes = current_user.organisation.groupes
+    else
+      @groupes = []
     end
   end
 end
