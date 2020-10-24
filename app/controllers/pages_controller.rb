b02d134c2e53605f4f8021dea198fffe790fe5d1
philnoug <philnoug@penguin>
2020-10-24 14:24:42 +0200

    page groupe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,26 @@
 class PagesController < ApplicationController
+
   def home
-    if current_user 
+    @groupes = []
+    if current_user
       @groupes = current_user.organisation.groupes
-    else
-      @groupes = []
+      unless params[:activité].blank? 
+        @groupes = @groupes.where(activité_id: params[:activité])
+      end
     end
   end
+
+  def groupe
+    @groupe = current_user.organisation.groupes.find(params[:groupe_id])
+
+    unless params[:rejoindre].blank?
+      unless @groupe.users.include?(current_user) 
+        @groupe.users << current_user
+        flash[:notice] = "Membre ajouté"
+      else
+        flash[:alert] = "Membre déjà inscrit !"
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   root 'pages#home'
+
+  get 'pages/groupe'
  
   devise_for :users
  
