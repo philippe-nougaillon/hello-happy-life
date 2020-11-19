4c01660e6f3b82c8cc817ba3e37fc7a8e5878a24
philnoug <philnoug@penguin>
2020-11-19 10:15:54 +0100

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ Activité.create([{ nom: "Running" }, { nom: "Yoga" }, { nom: "Marche nordique" 
 User.create(organisation_id: 1, admin: true, email: "philippe.nougaillon@gmail.com", password: "0123456", password_confirmation: "0123456")
 User.create(organisation_id: 1, admin: true, email: "richard.vonkat@gmail.com", password: "0123456", password_confirmation: "0123456")
 
-Groupe.create(organisation_id: 1, activité_id: 1, nom: "Running du dimanche")
+Groupe.create(organisation_id: 1, activité_id: 1, nom: "Running sur le parvis à midi")
 Groupe.create(organisation_id: 1, activité_id: 3, nom: "Marche en forêt")
 Groupe.create(organisation_id: 1, activité_id: 4, nom: "Chess-boxing Club")
 Groupe.create(organisation_id: 1, activité_id: 6, nom: "Vas-y, ponce !")
 Groupe.create(organisation_id: 1, activité_id: 2, nom: "Méditation Express")
-Groupe.create(organisation_id: 1, activité_id: 5, nom: "Country pour la vie")
+Groupe.create(organisation_id: 1, activité_id: 5, nom: "Salsa et danses latino, Rock, West Coast Swing, Line dance, Disco... Venez danser au soleil au pieds des oeuvres d'art !")
